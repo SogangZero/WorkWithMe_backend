231c774f5cb255ad673e23ad3356e875266f5d9a
Sanity1207 <newxxenson@naver.com>
2024-04-30 20:56:32 +0900

    initial commit

---
+++ src/main/java/com/wwme/wwme/WwmeApplication.java
@@ -0,0 +1,13 @@
+package com.wwme.wwme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwmeApplication.class, args);
+	}
+
+}
