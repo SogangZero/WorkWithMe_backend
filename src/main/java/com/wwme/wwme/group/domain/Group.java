6e463ce4bd7fdedb93a99d3c2bdcfa1698faac82
Seokmin Woo <clematis7593@gmail.com>
2024-05-07 02:02:20 +0900

    Fix: changed table of Group.java (group is reserved keyword in sql)

--- src/main/java/com/wwme/wwme/group/domain/Group.java
+++ src/main/java/com/wwme/wwme/group/domain/Group.java
@@ -1,15 +1,13 @@
 package com.wwme.wwme.group.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "group_table")
 public class Group {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
