580c7568e5d7864ec59c94d6ff83dfa033c81865
Seokmin Woo <clematis7593@gmail.com>
2024-05-07 03:02:31 +0900

    Test: Add config file for Test

---
+++ src/test/java/com/wwme/wwme/WwmeTestConfiguration.java
@@ -0,0 +1,10 @@
+package com.wwme.wwme;
+
+import com.wwme.wwme.group.repository.GroupRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WwmeTestConfiguration {
+
+}
