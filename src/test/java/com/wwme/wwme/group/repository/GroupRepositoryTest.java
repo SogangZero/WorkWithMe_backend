cb9b6207a226481649a9b72d56ffe22a96af7c20
Seokmin Woo <clematis7593@gmail.com>
2024-05-07 03:00:13 +0900

    Test: add test code for UserGroupRepository

--- src/test/java/com/wwme/wwme/group/repository/GroupRepositoryTest.java
+++ src/test/java/com/wwme/wwme/group/repository/GroupRepositoryTest.java
@@ -1,14 +1,58 @@
 package com.wwme.wwme.group.repository;
 
+import com.wwme.wwme.group.domain.Group;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class GroupRepositoryTest {
 
-    @Test
-    void groupAddTest() {
+    private final GroupRepository groupRepository;
 
+    @Autowired
+    public GroupRepositoryTest(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    @AfterEach
+    void tearDown() {
+        groupRepository.deleteAll();
+    }
+
+    @Test
+    void groupSaveTest() {
+        Group g1 = new Group("g1");
+        g1 = groupRepository.save(g1);
+
+        Group savedGroup = groupRepository.findById(g1.getId()).get();
+
+        assertThat(savedGroup.getId()).isEqualTo(g1.getId());
+    }
+
+    @Test
+    void groupRemoveTest() {
+        Group g1 = new Group("g1");
+        g1 = groupRepository.save(g1);
+
+        groupRepository.delete(g1);
+
+        long totalGroupCount = groupRepository.count();
+        assertThat(totalGroupCount).isEqualTo(0);
+    }
+
+    @Test
+    void groupFindByGroupNameTest() {
+        Group g1 = new Group("g1");
+        g1 = groupRepository.save(g1);
+
+        Group foundGroup = groupRepository.findByGroupName("g1").get();
+        assertThat(g1.getId()).isEqualTo(foundGroup.getId());
     }
 }
