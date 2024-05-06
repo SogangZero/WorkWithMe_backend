5cbfba328101192eff7c76a5208db490702a455d
Seokmin Woo <clematis7593@gmail.com>
2024-05-07 02:59:11 +0900

    Fix: make GroupRepository extend JpaRepository Fix: use lombok for generating constructor

--- src/main/java/com/wwme/wwme/group/domain/Group.java
+++ src/main/java/com/wwme/wwme/group/domain/Group.java
@@ -2,10 +2,12 @@ package com.wwme.wwme.group.domain;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "group_table")
 public class Group {
@@ -13,4 +15,8 @@ public class Group {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String groupName;
+
+    public Group(String groupName) {
+        this.groupName = groupName;
+    }
 }
--- src/main/java/com/wwme/wwme/group/repository/GroupRepository.java
+++ src/main/java/com/wwme/wwme/group/repository/GroupRepository.java
@@ -1,7 +1,12 @@
 package com.wwme.wwme.group.repository;
 
+import com.wwme.wwme.group.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface GroupRepository {
+public interface GroupRepository extends JpaRepository<Group, Long> {
+    Optional<Group> findByGroupName(String groupName);
 }
