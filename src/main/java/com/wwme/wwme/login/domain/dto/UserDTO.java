c8149d2305e79bd3cef3861c728faa903857f690
DongHyun <kimdh0315@naver.com>
2024-05-07 17:00:56 +0900

    feat : construct UserRepository and make domain class UserDTO and UserEntity

---
+++ src/main/java/com/wwme/wwme/login/domain/dto/UserDTO.java
@@ -0,0 +1,14 @@
+package com.wwme.wwme.login.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private String name;
+    private String username;
+    private String role;
+}
---
+++ src/main/java/com/wwme/wwme/login/domain/entity/UserEntity.java
@@ -0,0 +1,20 @@
+package com.wwme.wwme.login.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String name;
+
+    private String role;
+}
