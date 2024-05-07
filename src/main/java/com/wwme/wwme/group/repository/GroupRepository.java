c92c80975d8749e0edc3d7977926d92b658289b2
Seokmin Woo <clematis7593@gmail.com>
2024-05-07 19:22:45 +0900

    Fix: Delete unused import

--- src/main/java/com/wwme/wwme/group/repository/GroupRepository.java
+++ src/main/java/com/wwme/wwme/group/repository/GroupRepository.java
@@ -4,7 +4,6 @@ import com.wwme.wwme.group.domain.Group;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
--- src/test/java/com/wwme/wwme/group/repository/GroupRepositoryTest.java
+++ src/test/java/com/wwme/wwme/group/repository/GroupRepositoryTest.java
@@ -1,15 +1,12 @@
 package com.wwme.wwme.group.repository;
 
 import com.wwme.wwme.group.domain.Group;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class GroupRepositoryTest {
