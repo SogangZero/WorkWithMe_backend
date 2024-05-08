fd439715eebf94826dff138d2c020be89012ba8a
Seokmin Woo <clematis7593@gmail.com>
2024-05-08 17:01:57 +0900

    Test: fix UserGroupRepositoryTest

--- src/test/java/com/wwme/wwme/group/repository/UserGroupRepositoryTest.java
+++ src/test/java/com/wwme/wwme/group/repository/UserGroupRepositoryTest.java
@@ -3,32 +3,37 @@ package com.wwme.wwme.group.repository;
 import com.wwme.wwme.group.domain.Group;
 import com.wwme.wwme.group.domain.UserGroup;
 import com.wwme.wwme.user.domain.User;
-import org.assertj.core.api.Assertions;
+import com.wwme.wwme.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class UserGroupRepositoryTest {
     private final UserGroupRepository userGroupRepository;
     private final GroupRepository groupRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserGroupRepositoryTest(
             UserGroupRepository userGroupRepository,
-            GroupRepository groupRepository
+            GroupRepository groupRepository,
+            UserRepository userRepository
     ) {
         this.userGroupRepository = userGroupRepository;
         this.groupRepository = groupRepository;
+        this.userRepository = userRepository;
     }
 
     @Test
     void userGroupSaveTest() {
         Group g1 = new Group("group111");
-        User u1 = new User();
+        g1 = groupRepository.save(g1);
+        User u1 = new User("user1");
+        u1 = userRepository.save(u1);
+
         UserGroup ug1 = new UserGroup();
         ug1.setGroup(g1);
         ug1.setUser(u1);
