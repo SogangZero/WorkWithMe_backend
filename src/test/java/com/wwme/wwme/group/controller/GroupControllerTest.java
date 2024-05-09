a419a49ad7cea5ff5a69c4b028b31b7ddf657b09
Seokmin Woo <clematis7593@gmail.com>
2024-05-09 21:37:40 +0900

    Test: update test to handle JWT

--- src/test/java/com/wwme/wwme/group/controller/GroupControllerTest.java
+++ src/test/java/com/wwme/wwme/group/controller/GroupControllerTest.java
@@ -3,14 +3,21 @@ package com.wwme.wwme.group.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wwme.wwme.group.DTO.GroupCreateRequestDTO;
 import com.wwme.wwme.group.DTO.GroupCreateSuccessResponseDTO;
+import com.wwme.wwme.group.domain.Group;
+import com.wwme.wwme.group.service.GroupService;
+import com.wwme.wwme.user.domain.User;
+import com.wwme.wwme.user.service.UserService;
+import jakarta.servlet.http.Cookie;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -30,6 +37,12 @@ class GroupControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private GroupService groupService;
+
+    @MockBean
+    private UserService userService;
+
     @Test
     void contextLoads() {
         assertThat(groupController).isNotNull();
@@ -37,17 +50,32 @@ class GroupControllerTest {
 
     @Test
     void groupCreateSuccess() throws Exception{
+        long groupId = 0L;
+        String groupName = "group_name";
+        String groupColor = "FFFFFF";
+        String jwtString = "exampleJWTString";
 
         GroupCreateRequestDTO requestDTO =
-                new GroupCreateRequestDTO("group_name", "FFFFFF");
+                new GroupCreateRequestDTO(groupName, groupColor);
 
         GroupCreateSuccessResponseDTO responseDTO =
-                new GroupCreateSuccessResponseDTO(true, 0, "temptemp");
+                new GroupCreateSuccessResponseDTO(true, groupId, "temptemp");
 
         final String request = objectMapper.writeValueAsString(requestDTO);
         final String response = objectMapper.writeValueAsString(responseDTO);
 
+        User mockedUser = new User();
+        when(userService.getUserFromJWTString(jwtString))
+                .thenReturn(mockedUser);
+
+        Group mockedGroup = new Group();
+        mockedGroup.setGroupName(requestDTO.getGroupName());
+        mockedGroup.setId(groupId);
+        when(groupService.createGroupWithUserAndColor(groupName, mockedUser, groupColor))
+                .thenReturn(mockedGroup);
+
         mockMvc.perform(post("/group")
+                        .cookie(new Cookie("Authorization", jwtString))
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(request))
                 .andDo(print())
