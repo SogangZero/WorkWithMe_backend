31a2d070487b4d2076c2e088b92c6d5d68c30c31
Sanity1207 <newxxenson@naver.com>
2024-05-10 12:21:29 +0900

    feat : add domain entity for Tag

--- src/main/java/com/wwme/wwme/task/domain/Tag.java
+++ src/main/java/com/wwme/wwme/task/domain/Tag.java
@@ -1,5 +1,6 @@
 package com.wwme.wwme.task.domain;
 
+import com.wwme.wwme.group.domain.Group;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -17,8 +18,11 @@ public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private Long group_id;
+
     private String tag_name;
+
+    @ManyToOne
+    private Group group;
 
     @OneToMany(mappedBy = "tag", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<Task> task_list;
