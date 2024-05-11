b66befd1d1ca7460c2d6df44b28f8c611ec78103
DongHyun <kimdh0315@naver.com>
2024-05-12 01:17:06 +0900

    feat : save register date into DB.

--- src/main/java/com/wwme/wwme/login/service/NicknameService.java
+++ src/main/java/com/wwme/wwme/login/service/NicknameService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -20,5 +22,6 @@ public class NicknameService {
 
         user.setNickname(nickname);
         user.setRole("ROLE_USER");
+        user.setRegistrationDate(LocalDateTime.now());
     }
 }
