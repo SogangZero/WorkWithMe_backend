db29bec3df624ac394304807407badbe891784cf
Seokmin Woo <clematis7593@gmail.com>
2024-05-15 00:00:56 +0900

    Test: add @Transactional tag to test

--- src/test/java/com/wwme/wwme/group/repository/UserGroupRepositoryTest.java
+++ src/test/java/com/wwme/wwme/group/repository/UserGroupRepositoryTest.java
@@ -7,10 +7,12 @@ import com.wwme.wwme.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class UserGroupRepositoryTest {
     private final UserGroupRepository userGroupRepository;
     private final GroupRepository groupRepository;
