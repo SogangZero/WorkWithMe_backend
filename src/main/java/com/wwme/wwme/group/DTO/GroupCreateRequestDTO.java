e34ad20465f8b490772a67fd803104fe6c8b8d29
DongHyun <kimdh0315@naver.com>
2024-05-15 01:19:48 +0900

    fix : add @NoArgsConstructor annotation at GroupCreateRequestDTO.java

--- src/main/java/com/wwme/wwme/group/DTO/GroupCreateRequestDTO.java
+++ src/main/java/com/wwme/wwme/group/DTO/GroupCreateRequestDTO.java
@@ -3,9 +3,11 @@ package com.wwme.wwme.group.DTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GroupCreateRequestDTO {
     @JsonProperty("group_name")
     String groupName;
