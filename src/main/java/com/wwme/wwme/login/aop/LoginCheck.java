ff43d16919c763492b580c95c847ac8bee2a38f9
DongHyun <kimdh0315@naver.com>
2024-05-28 17:17:45 +0900

    feat : check whether request user is logged in by method annotation @LoginCheck

---
+++ src/main/java/com/wwme/wwme/login/aop/LoginCheck.java
@@ -0,0 +1,12 @@
+package com.wwme.wwme.login.aop;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface LoginCheck {
+    String checkLevel() default "ROLE_USER";
+}
