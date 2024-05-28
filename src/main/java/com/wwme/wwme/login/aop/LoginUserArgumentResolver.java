d1b04293ad907ebdb6c7b5ab4af3c57728ac6365
DongHyun <kimdh0315@naver.com>
2024-05-28 17:18:28 +0900

    feat : return User instance whether request user is logged in by parameter annotation @Login

---
+++ src/main/java/com/wwme/wwme/login/aop/LoginUserArgumentResolver.java
@@ -0,0 +1,40 @@
+package com.wwme.wwme.login.aop;
+
+import com.wwme.wwme.login.jwt.JWTUtil;
+import com.wwme.wwme.user.domain.User;
+import com.wwme.wwme.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+
+@RequiredArgsConstructor
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+    private final UserRepository userRepository;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(Login.class)
+                && User.class.isAssignableFrom(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter,
+                                  ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest,
+                                  WebDataBinderFactory binderFactory)
+            throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        String accessToken = request.getHeader("access");
+        String userKey = jwtUtil.getUserKey(accessToken);
+
+        User user = userRepository.findByUserKey(userKey).orElse(null);
+
+        return user;
+    }
+}
