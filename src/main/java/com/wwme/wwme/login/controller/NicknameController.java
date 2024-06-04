6eb300a474b00ab125451619d993cf17d75f7fe1
DongHyun <kimdh0315@naver.com>
2024-06-04 19:18:58 +0900

    feat : user nickname logic

--- src/main/java/com/wwme/wwme/login/controller/NicknameController.java
+++ src/main/java/com/wwme/wwme/login/controller/NicknameController.java
@@ -1,41 +1,51 @@
 package com.wwme.wwme.login.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wwme.wwme.login.domain.dto.NicknameDTO;
+import com.wwme.wwme.login.domain.dto.SuccessDTO;
+import com.wwme.wwme.login.exception.JwtTokenException;
 import com.wwme.wwme.login.jwt.JWTUtil;
 import com.wwme.wwme.login.service.NicknameService;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.io.IOException;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class NicknameController {
     private final NicknameService nicknameService;
     private final JWTUtil jwtUtil;
+    private final ObjectMapper objectMapper;
 
-    @GetMapping("/login/nickname")
-    public String nickName() {
-        return "login/nickname";
-    }
 
     @PostMapping("/login/nickname")
-    public String saveNickname(HttpServletRequest request,
-                               @RequestBody() Map<String, String> param) {
-        String nickname = param.get("nickname");
-        String authorization = request.getHeader("access");
-        String userKey = jwtUtil.getUserKey(authorization);
+    public void saveNickname(HttpServletRequest request,
+                             HttpServletResponse response,
+                             @RequestBody NicknameDTO nicknameDTO)
+            throws IOException {
+
         try {
-            nicknameService.saveNicknameAndChangeRole(nickname, userKey);
-        } catch (IllegalArgumentException e) {
-            return "redirect:/login/nickname";
+            String nickname = nicknameDTO.getNickname();
+            String authorization = request.getHeader("access");
+            String userKey = jwtUtil.getUserKey(authorization);
+            log.info("User[{}] request nickname setting by {}", userKey, nickname);
+
+            nicknameService.saveNicknameAndChangeRole(nickname, "ROLE_USER", userKey);
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("utf-8");
+            SuccessDTO successDTO = new SuccessDTO(true);
+            String result = objectMapper.writeValueAsString(successDTO);
+            response.getWriter().write(result);
+
+        } catch (IllegalArgumentException |JwtTokenException e) {
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         }
 
-        log.info("User[{}] request nickname setting by {}", userKey, nickname);
-
-        return "redirect:/";
     }
 }
