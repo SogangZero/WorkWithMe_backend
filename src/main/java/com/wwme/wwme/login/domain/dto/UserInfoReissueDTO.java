f5ab9d8823fb45aec97aeec330ac42c75cfd36de
DongHyun <kimdh0315@naver.com>
2024-06-25 21:42:13 +0900

    fix : add @Data annotation at UserInfoReissueDTO

--- src/main/java/com/wwme/wwme/login/domain/dto/UserInfoReissueDTO.java
+++ src/main/java/com/wwme/wwme/login/domain/dto/UserInfoReissueDTO.java
@@ -2,11 +2,13 @@ package com.wwme.wwme.login.domain.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Data
 public class UserInfoReissueDTO {
     private Long user_id;
     private String nickname;
