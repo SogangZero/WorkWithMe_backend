2405b61971f17cf9324913df1c0d832e339b8a9f
DongHyun <kimdh0315@naver.com>
2024-06-25 21:25:20 +0900

    fix : reissue get refresh token from request

--- src/main/java/com/wwme/wwme/login/controller/ReissueController.java
+++ src/main/java/com/wwme/wwme/login/controller/ReissueController.java
@@ -34,22 +34,14 @@ public class ReissueController {
     public ResponseEntity<?> reissue(HttpServletRequest request,
                                      HttpServletResponse response) {
         try {
-            log.info("start /reissue");
-            String refreshToken = response.getHeader("refresh");
-            log.info("refresh1 {}", refreshToken);
+            String refreshToken = request.getHeader("refresh");
             String refresh = reissueService.validateRefreshToken(refreshToken);
-            log.info("refresh2 {}", refresh);
             String userKey = jwtUtilService.getUserKey(refresh);
-            log.info("userkey {}", userKey);
             String role = jwtUtilService.getRole(refresh);
-            log.info("role {}", role);
             String newAccess = reissueService.generateAccessToken(userKey, role);
-            log.info("newAccess {}", newAccess);
             String newRefresh = reissueService.exchangeRefreshToken(userKey, role, refresh);
-            log.info("newRefresh {}", newRefresh);
             User user = userRepository.findByUserKey(userKey)
                     .orElseThrow(() -> new IllegalArgumentException("not exist user"));
-            log.info("user id {}", user.getId());
             UserInfoReissueDTO userInfoReissue = userService.getUserInfoReissue(user);
 
             log.info("User[{}] re-generate access and refresh token", userKey);
