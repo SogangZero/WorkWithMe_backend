88f101ed117694bdbb845a1b9feaafa119f0df2c
Seokmin Woo <clematis7593@gmail.com>
2024-07-02 18:15:25 +0900

    Fix: fix logic for when tagList is null

--- src/main/java/com/wwme/wwme/task/service/TaskCRUDServiceImpl.java
+++ src/main/java/com/wwme/wwme/task/service/TaskCRUDServiceImpl.java
@@ -619,14 +619,14 @@ public class TaskCRUDServiceImpl implements TaskCRUDService {
         }
 
         if (startDate == null)
-            startDate = LocalDateTime.of(1999,12,7,12,12,12);
+            startDate = LocalDateTime.of(1999,12,12,12,12,12);
         if (endDate == null)
-            endDate = LocalDateTime.of(2099,12,7,12,12,12);
+            endDate = LocalDateTime.of(2099,12,12,12,12,12);
 
         // only display tasks without due date
         if (!withDueDate) {
-            startDate = LocalDateTime.of(2099,12,7,12,12,12);
-            endDate = LocalDateTime.of(2099,12,7,12,12,12);
+            startDate = LocalDateTime.of(2099,12,12,12,12,12);
+            endDate = LocalDateTime.of(2099,12,12,12,12,12);
         }
 
         Task lastTask;
