c04c21d7083d9ca74e265d01d86e2f61944626aa
DongHyun <kimdh0315@naver.com>
2024-07-08 20:52:58 +0900

    chore : catch OAuth2AuthorizationException and re-throw

--- src/main/java/com/wwme/wwme/login/service/CustomOAuth2UserService.java
+++ src/main/java/com/wwme/wwme/login/service/CustomOAuth2UserService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthorizationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
@@ -68,6 +69,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
             userRepository.save(user);
             return new CustomOAuth2User(userDTO);
+        } catch (OAuth2AuthorizationException e) {
+            log.error(e.getMessage());
+            throw e;
         }
     }
 
