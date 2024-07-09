2c2a06032fbc285ff4d13a1bbe4a52d8c3ab5700
DongHyun <kimdh0315@naver.com>
2024-07-09 18:12:24 +0900

    feat : set timezone at Asia/Seoul

--- src/main/java/com/wwme/wwme/WwmeApplication.java
+++ src/main/java/com/wwme/wwme/WwmeApplication.java
@@ -1,12 +1,20 @@
 package com.wwme.wwme;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class WwmeApplication {
+
+	@PostConstruct
+	public void timeSet() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WwmeApplication.class, args);
