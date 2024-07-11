4dc10ccc573e96c56a063588e4c750df583e6d09
DongHyun <kimdh0315@naver.com>
2024-07-11 15:09:34 +0900

    chore : delete un-used function

--- src/main/java/com/wwme/wwme/login/controller/ReissueController.java
+++ src/main/java/com/wwme/wwme/login/controller/ReissueController.java
@@ -57,12 +57,4 @@ public class ReissueController {
             return new ResponseEntity<>(errorDTO, HttpStatus.BAD_REQUEST);
         }
     }
-
-    private Cookie createCookie(String key, String value) {
-        Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24 * 60 * 60);
-        cookie.setHttpOnly(true);
-
-        return cookie;
-    }
 }
